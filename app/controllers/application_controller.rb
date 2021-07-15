80014b2b73bd0a0ad2d69ebebb826e45b76ebb4d
s-b-edu <sbaur@chicagobooth.edu>
2021-07-15 01:59:44 +0000

    add and subtract done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-
+  def addition_results
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @result = @first + @second
+      render("add_templates/results.html.erb")
+  end
+  def addition
+      render("add_templates/input.html.erb")
+  end
+  def subtraction_results
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @result = @second - @first
+      render("subtract_templates/results.html.erb")
+  end
+  def subtraction
+      render("subtract_templates/input.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/wizard_add", { :controller => "application", :action => "addition_results"})
+  get("/add", { :controller => "application", :action => "addition"})
+  get("/wizard_subtract", { :controller => "application", :action => "subtraction_results"})
+  get("/subtract", { :controller => "application", :action => "subtraction"})
 end

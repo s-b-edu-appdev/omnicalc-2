4e85a6640f6ed550c5bd1dde904205d8e87f3971
s-b-edu <sbaur@chicagobooth.edu>
2021-07-18 13:29:32 +0000

    done with weather and address work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,18 +51,44 @@ class ApplicationController < ActionController::Base
     render("extra_templates/stc_results.html.erb")
   end
   def ctw_input
-      @lat = params.fetch("lat").to_f
-      @lng = params.fetch("lng").to_f
-      render("extra_templates/ctw_input.html.erb")
+    render("extra_templates/ctw_input.html.erb")
   end
   def ctw_results
-      render("extra_templates/ctw_results.html.erb")
+    @lat = params.fetch("lat").to_f
+    @lng = params.fetch("lng").to_f
+    weather_url = "https://api.darksky.net/forecast/#{ENV.fetch("DARK_SKY_KEY")}/#{@lat},#{@lng}"
+    parsed_weather = JSON.parse(open(weather_url).read)
+    current = parsed_weather.fetch("currently")
+    @temp = current.fetch("temperature")
+    @summary = current.fetch("summary")
+    hourly = parsed_weather.fetch("hourly")
+    @outlook_60min = parsed_weather.dig("minutely","summary")
+    @outlook_hours = parsed_weather.dig("hourly","summary")
+    @outlook_days = parsed_weather.dig("daily","summary")
+    render("extra_templates/ctw_results.html.erb")
   end
   def stw_input
-      @add_in = params.fetch("add_in")
-      render("extra_templates/stw_input.html.erb")
+    render("extra_templates/stw_input.html.erb")
   end
   def stw_results
-      render("extra_templates/stw_results.html.erb")
+    @add_in = params.fetch("add_in")
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@add_in}&key=" + ENV.fetch("GMAPS_KEY")
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    results = parsed_data.fetch("results").at(0)
+    geometry = results.fetch("geometry")
+    location = geometry.fetch("location")
+    @lat = location.fetch("lat")
+    @lng = location.fetch("lng")
+    weather_url = "https://api.darksky.net/forecast/#{ENV.fetch("DARK_SKY_KEY")}/#{@lat},#{@lng}"
+    parsed_weather = JSON.parse(open(weather_url).read)
+    current = parsed_weather.fetch("currently")
+    @temp = current.fetch("temperature")
+    @summary = current.fetch("summary")
+    hourly = parsed_weather.fetch("hourly")
+    @outlook_60min = parsed_weather.dig("minutely","summary")
+    @outlook_hours = parsed_weather.dig("hourly","summary")
+    @outlook_days = parsed_weather.dig("daily","summary")
+    render("extra_templates/stw_results.html.erb")
   end
 end
